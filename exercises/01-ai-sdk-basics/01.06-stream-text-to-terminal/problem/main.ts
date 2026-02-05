553522b542e59697dacf53ff9fb863d663d348be
kMlad <kmlad@kmlad.dev>
2026-02-05 16:19:54 +0100

    Complete Streaming Text Exercise

--- exercises/01-ai-sdk-basics/01.06-stream-text-to-terminal/problem/main.ts
+++ exercises/01-ai-sdk-basics/01.06-stream-text-to-terminal/problem/main.ts
@@ -1,12 +1,12 @@
-import { google } from '@ai-sdk/google';
 import { streamText } from 'ai';
-
-const model = google('gemini-2.5-flash');
 
 const prompt =
   'Give me the first paragraph of a story about an imaginary planet.';
 
-const stream = TODO; // TODO - stream some text with the model above.
+const stream = streamText({
+  model: 'google/gemini-2.0-flash-lite',
+  prompt,
+});
 
 for await (const chunk of stream.textStream) {
   process.stdout.write(chunk);
