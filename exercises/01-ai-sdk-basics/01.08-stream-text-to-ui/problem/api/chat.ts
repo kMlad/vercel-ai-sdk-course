909f9d14f244a0479f52907384267d5979c7aad9
kMlad <kmlad@kmlad.dev>
2026-02-05 17:24:14 +0100

    Solve streaming text to the UI

--- exercises/01-ai-sdk-basics/01.08-stream-text-to-ui/problem/api/chat.ts
+++ exercises/01-ai-sdk-basics/01.08-stream-text-to-ui/problem/api/chat.ts
@@ -1,5 +1,5 @@
-import { google } from '@ai-sdk/google';
 import {
+  convertToModelMessages,
   createUIMessageStreamResponse,
   streamText,
   type ModelMessage,
@@ -7,21 +7,22 @@ import {
 } from 'ai';
 
 export const POST = async (req: Request): Promise<Response> => {
-  const body = await req.json();
+  const body: {
+    messages: UIMessage[];
+  } = await req.json();
+  console.log(body);
 
-  // TODO: get the UIMessage[] from the body
-  const messages: UIMessage[] = TODO;
+  const messages: UIMessage[] = body.messages;
 
-  // TODO: convert the UIMessage[] to ModelMessage[]
-  const modelMessages: ModelMessage[] = TODO;
+  const modelMessages: ModelMessage[] =
+    await convertToModelMessages(messages);
 
-  // TODO: pass the modelMessages to streamText
   const streamTextResult = streamText({
-    model: google('gemini-2.5-flash'),
+    model: 'google/gemini-2.0-flash',
+    messages: modelMessages,
   });
 
-  // TODO: create a UIMessageStream from the streamTextResult
-  const stream = TODO;
+  const stream = streamTextResult.toUIMessageStream();
 
   return createUIMessageStreamResponse({
     stream,
