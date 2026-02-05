ef4a38b397f163a3be86eeee045b03d1277305f2
kMlad <kmlad@kmlad.dev>
2026-02-05 18:08:32 +0100

    Generate objects/structured output

--- exercises/01-ai-sdk-basics/01.11-generating-objects-via-output/problem/main.ts
+++ exercises/01-ai-sdk-basics/01.11-generating-objects-via-output/problem/main.ts
@@ -1,11 +1,8 @@
-import { google } from '@ai-sdk/google';
-import { streamText } from 'ai';
+import { generateText, Output, streamText } from 'ai';
 import z from 'zod';
 
-const model = google('gemini-2.5-flash');
-
 const stream = streamText({
-  model,
+  model: 'google/gemini-2.0-flash',
   prompt:
     'Give me the first paragraph of a story about an imaginary planet.',
 });
@@ -16,15 +13,19 @@ for await (const chunk of stream.textStream) {
 
 const finalText = await stream.text;
 
-// TODO: Replace this with a call to generateText, passing:
-// - The model, same as above
-// - The prompt, asking for facts about the imaginary planet,
-//   passing in the finalText as the story
-// - The output, which should be Output.object({}), passing
-//   the schema: z.object({
-//     facts: z.array(z.string()).describe('The facts about the imaginary planet. Write as if you are a scientist.'),
-//   })
-const factsResult = TODO;
+const factsResult = await generateText({
+  model: 'google/gemini-2.0-flash',
+  prompt: `Give me some facts about this planet: \n ${finalText}`,
+  output: Output.object({
+    schema: z.object({
+      facts: z
+        .array(z.string())
+        .describe(
+          'The facts about the imaginary planet. Describe as if you are a scientist',
+        ),
+    }),
+  }),
+});
 
 // TODO: Log the output of the result
 console.log(factsResult.output);
