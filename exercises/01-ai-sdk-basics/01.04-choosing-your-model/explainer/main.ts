294e27334a77d46c809134748c9547a1f1a14a22
kMlad <kmlad@kmlad.dev>
2026-02-05 16:05:41 +0100

    Initial commit

--- exercises/01-ai-sdk-basics/01.04-choosing-your-model/explainer/main.ts
+++ exercises/01-ai-sdk-basics/01.04-choosing-your-model/explainer/main.ts
@@ -1,12 +1,3 @@
-// Requires an OPENAI_API_KEY environment variable in .env
-import { openai } from '@ai-sdk/openai';
+import modelProvider from 'ai'
 
-// Requires a GOOGLE_GENERATIVE_AI_API_KEY environment variable in .env
-import { google } from '@ai-sdk/google';
-
-// Requires an ANTHROPIC_API_KEY environment variable in .env
-import { anthropic } from '@ai-sdk/anthropic';
-
-const model = openai('gpt-4o-mini');
-
-console.dir(model, { depth: null });
+console.dir(modelProvider, { depth: null });
